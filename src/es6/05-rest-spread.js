3bf6dca5cf5eb6800ea2868b5e95910c6f82e564
Mario Riascos <marioriascos1201@gmail.com>
2023-02-10 11:23:39 -0500

    ES6: Spread Operator

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -1,19 +1,41 @@
 //Arrays destructuring 
 
-let fruits = ['Apple','Banana'];
+let fruits = ['Apple', 'Banana'];
 
-let [a,b] = fruits
+let [a, b] = fruits
 
-console.log(a,b);
+console.log(a, b);
 
 //Object destructuring
 
 let user = {
-    username:'Oscar',
+    username: 'Oscar',
     age: 34
 };
 
-let {username, age} = user;
+let { username, age } = user;
 
-console.log(username,age);
+console.log(username, age);
+
+//! Spread Operator
+
+let person = { name: 'Oscar', age: 28 };
+let country = 'MX';
+
+let data = {id:1, ...person, country };
+
+console.log(data)
+
+//! REST
+
+function sum(num, ...values){
+    console.log(values);
+    console.log(num+values[0]);
+    return num + values[0]
+}
+
+sum(1,1,2,3);
+
+
+sum()
 
